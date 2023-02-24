c0a3b920bbe6f9d3beeeffb44682363fe46f8731
Igrok82 <pplazy1982@gmail.com>
2023-02-24 21:37:34 +0300

    start TS input interface

--- app.vue
+++ app.vue
@@ -6,29 +6,52 @@
       <div class="option-container">
         <h4>1) Chose a gender</h4>
         <div class="options-buttons">
-          <button>Boy</button>
-          <button>Unisex</button>
-          <button>Girl</button>
+          <button @click="options.gender = 'Boy'" class="btn"
+            :class="options.gender === 'Boy' && 'options-active'">Boy</button>
+          <button @click="options.gender = 'Unisex'" class="btn"
+            :class="options.gender === 'Unisex' && 'options-active'">Unisex</button>
+          <button @click="options.gender = 'Girl'" class="btn"
+            :class="options.gender === 'Girl' && 'options-active'">Girl</button>
         </div>
       </div>
       <div class="option-container">
         <h4>2) Choose the name's popularity</h4>
         <div class="options-buttons">
-          <button>Trendy</button>
-          <button>Unique</button>
+          <button @click="options.popularity = 'Trendy'" class="btn"
+            :class="options.popularity === 'Trendy' && 'options-active'">Trendy</button>
+          <button @click="options.popularity = 'Unique'" class="btn"
+            :class="options.popularity === 'Unique' && 'options-active'">Unique</button>
         </div>
       </div>
       <div class="option-container">
         <h4>3) Choose name's length</h4>
         <div class="options-buttons">
-          <button>Long</button>
-          <button>All</button>
-          <button>Short</button>
+          <button @click="options.length = 'Long'" class="btn"
+            :class="options.length === 'Long' && 'options-active'">Long</button>
+          <button @click="options.length = 'All'" class="btn"
+            :class="options.length === 'All' && 'options-active'">All</button>
+          <button @click="options.length = 'Short'" class="btn"
+            :class="options.length === 'Short' && 'options-active'">Short</button>
         </div>
       </div>
     </div>
   </div>
 </template>
+
+<script lang="ts" setup>
+
+interface OptinsState {
+  gender: string;
+  popularity: string;
+  length: string;
+}
+
+const options: OptinsState = reactive({
+  gender: "Girl",
+  popularity: "Unique",
+  length: "Short"
+})
+</script>
 
 <style scoped>
 .container {
@@ -47,10 +70,37 @@
 
 .optiions-container {
   background-color: rgb(255, 238, 236);
+  border-radius: 2rem;
+  padding: 1rem;
+  width: 95%;
+  margin: 0 auto;
+  margin-top: 4rem;
+  position: relative;
 
 }
 
-.option-container {}
+.option-container {
+  margin-bottom: 2rem;
+}
 
-.options-buttons {}
-</style>
+.btn {
+  background: white;
+  outline: 0.15rem solid rgb(249, 87, 89);
+  padding: 0.75rem;
+  width: 12rem;
+  font-size: 1rem;
+  color: rgb(27, 60, 138);
+  cursor: pointer;
+  font-weight: 200;
+  margin: 1rem;
+  border-radius: 1.25rem;
+
+}
+
+.options-active {
+  background-color: #ff6262;
+  color: white;
+
+}
+
+.options-buttons {}</style>
